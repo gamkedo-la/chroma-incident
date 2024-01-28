be5fd0d9d8dc842f9f74ecd202ca8d2e0d68a9f0
Chris L <hypedoncoffee@gmail.com>
2024-01-28 03:24:41 -0500

    changed: Enemies can fire missiles now, but their missiles are hurting themselves. May need to assign missile owners or change spawn location on enemy sprite

--- Actors/Enemy/Enemy.gd
+++ Actors/Enemy/Enemy.gd
@@ -11,9 +11,20 @@ var alive:bool = true
 var target = Global.player
 @onready var navigation_agent: NavigationAgent2D = $Navigation/NavigationAgent2D
 
+var fire_rate_per_second: int = 4
+
 func _ready():
+	var child_node_health = get_node('Health')  # Connect to our health component
+	# Connect the health component signal for health to our handler in enemy
+	child_node_health.healthReachedMinimum.connect(_on_healthReachedMinimum)
+
 	if Global.player:
 		target = Global.player
+
+func _on_healthReachedMinimum():
+	alive = false
+	print("Enemy's been killed")
+	animation_player.play("die")
 
 func _physics_process(_delta):
 	var direction = Vector2.ZERO
@@ -25,13 +36,14 @@ func _physics_process(_delta):
 	look_at(target.global_position)
 	if alive:
 		move_and_slide()
+	var projectile_resource:ProjectileBase = Global.bullet_types[0]
+	SignalBus.emit_fire(projectile_resource, global_position,
+		(target.global_position))
 
 func _on_timer_timeout():
 	navigation_agent.target_position = target.global_position
 
 func handle_hit():
-	print("Enemy's been hit")
-	alive = false
-	animation_player.play("die")
+	$Health.take_damage(5)
 	
 	
---
+++ scripts/HealthComponent.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var current_health : int = 100
+var max_health : int = 100
+var min_health : int = 0
+
+signal healthReachedMinimum()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func take_damage(damage: int):
+	current_health -= damage
+	current_health = max(current_health, min_health)
+	if (current_health == min_health):
+		emit_signal("healthReachedMinimum")
